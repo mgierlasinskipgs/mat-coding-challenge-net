8a7deb1a113afc1b89a4448f251480e46271fb2d
Marcin Gierlasiński <mgierlasinski@pgs-soft.com>
2019-07-14 15:10:01 +0200

    Extension - convert unix timestamp do DateTime

---
+++ CodingChallenge/CodingChallenge/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CodingChallenge
+{
+    public static class Extensions
+    {
+        public static DateTime ToDateTime(this double unixTimeStamp)
+        {
+            var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+    }
+}
