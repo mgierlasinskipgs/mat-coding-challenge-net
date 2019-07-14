9965bcc4952011b45c9fe1876ffb8411663868b8
Marcin Gierlasiński <mgierlasinski@pgs-soft.com>
2019-07-14 14:01:25 +0200

    Create basic console project

---
+++ CodingChallenge/CodingChallenge/MqttApp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    public class MqttApp
+    {
+        public async Task RunAsync()
+        {
+            await Task.Delay(500);
+
+            Console.WriteLine("App started");
+        }
+    }
+}
---
+++ CodingChallenge/CodingChallenge/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CodingChallenge
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var app = new MqttApp();
+            await app.RunAsync();
+
+            Console.ReadLine();
+        }
+    }
+}
